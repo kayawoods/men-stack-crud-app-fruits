11c1841f9d129b35c848d97816ab3767ba4f3ec5
Kaya Woods <kayanevada@gmail.com>
2025-02-14 09:43:23 -0800

    configure the app for mongodb connection

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+const dotenv = require('dotenv')
+dotenv.config() 
+
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose')  //importing packages
+
+const app = express(); //when we import express, we must call express 
+
+mongoose.connect(process.env.MONGODB_URI) //process.env is looking into env file and looking for variable called MONGODB_URI
+
+mongoose.connection.on('connected', () => {
+    console.log(`Connected on MongoDB ${mongoose.connection.name}`) //shows we are connected to MongoDB atlas
+})
+
+
+
+
 
 // GET
 app.get('/', async(req, res) => {
