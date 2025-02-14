070776cb6f9a0c27f579c21e3ae98848fb09ce5d
Kaya Woods <kayanevada@gmail.com>
2025-02-14 14:26:41 -0800

    can edit

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const dotenv = require('dotenv')
-dotenv.config() 
+dotenv.config()
 
 const express = require('express');
-const mongoose = require('mongoose')  //importing packages
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const morgan = require('morgan') //importing packages
 
 const app = express(); //when we import express, we must call express 
 
@@ -16,18 +18,20 @@ const Fruit = require('./models/fruit.js')
 
 // middleware
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'))
+app.use(morgan('dev'))
 
 
 // GET
-app.get('/', async(req, res) => {
-res.render('index.ejs')
+app.get('/', async (req, res) => {
+    res.render('index.ejs')
 })
 
 //GET/ fruits 
-app.get('/fruits', async(req, res) => {
+app.get('/fruits', async (req, res) => {
     const allFruits = await Fruit.find({})
     console.log(allFruits)
-    res.render('fruits/index.ejs', { fruits: allFruits})
+    res.render('fruits/index.ejs', { fruits: allFruits })
 })
 
 // GET /fruits/new 
@@ -35,19 +39,34 @@ app.get('/fruits/new', (req, res) => {
     res.render('fruits/new.ejs')
 })
 
+// Get/fruits/:id
+app.get('/fruits/:fruitId', async (req, res) => {
+    const foundFruit = await Fruit.findById(req.params.fruitId)
+    res.render("fruits/show.ejs", { fruit: foundFruit });
+})
 // POST/fruits 
 app.post('/fruits', async (req, res) => {
     if (req.body.isReadyToEat === "on") {
         req.body.isReadyToEat = true;
-      } else {
+    } else {
         req.body.isReadyToEat = false;
-      }
+    }
 
-      await Fruit.create(req.body);
-      res.redirect("/fruits");
-    })
+    await Fruit.create(req.body);
+    res.redirect("/fruits");
+})
+app.delete("/fruits/:fruitId", async (req, res) => {
+    await Fruit.findByIdAndDelete(req.params.fruitId);
+    res.redirect("/fruits");
+});
 
+app.get("/fruits/:fruitId/edit", async (req, res) => {
+    const foundFruit = await Fruit.findById(req.params.fruitId);
+    console.log(foundFruit);
+    res.render('fruits/edit.ejs', { fruit: foundFruit}) 
+  });
+  
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000');
+    console.log('Listening on port 3000');
 });
