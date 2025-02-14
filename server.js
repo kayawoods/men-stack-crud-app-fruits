57d21ab9dd9196686f58bd8b19f02eb8b6082bb0
Kaya Woods <kayanevada@gmail.com>
2025-02-14 12:17:49 -0800

    add create functionality

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ mongoose.connection.on('connected', () => {
 
 const Fruit = require('./models/fruit.js')
 
+// middleware
+app.use(express.urlencoded({ extended: false }));
 
 
 // GET
@@ -25,6 +27,19 @@ res.render('index.ejs')
 app.get('/fruits/new', (req, res) => {
     res.render('fruits/new.ejs')
 })
+
+// POST/fruits 
+app.post('/fruits', async (req, res) => {
+    if (req.body.isReadyToEat === "on") {
+        req.body.isReadyToEat = true;
+      } else {
+        req.body.isReadyToEat = false;
+      }
+      
+      await Fruit.create(req.body);
+      res.redirect("/fruits/new");
+    })
+
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
