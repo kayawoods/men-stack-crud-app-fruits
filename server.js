20dcf4837245d8cd0914bbcd38c4e7a17fc9f3da
Kaya Woods <kayanevada@gmail.com>
2025-02-14 14:36:26 -0800

    finished the lecture

--- server.js
+++ server.js
@@ -65,6 +65,16 @@ app.get("/fruits/:fruitId/edit", async (req, res) => {
     console.log(foundFruit);
     res.render('fruits/edit.ejs', { fruit: foundFruit}) 
   });
+
+  app.put("/fruits/:fruitId", async (req, res) => {
+    if (req.body.isReadyToEat === "on") {
+        req.body.isReadyToEat = true;
+      } else {
+        req.body.isReadyToEat = false;
+      }
+      await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+      res.redirect(`/fruits/${req.params.fruitId}`);
+    });
   
 
 app.listen(3000, () => {
